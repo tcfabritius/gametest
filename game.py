ceaa8311ceb1223039ec1977b38bdda6cfd3753f
Tim Fabritius <tim.fabritius@skiff.com>
2024-09-19 10:05:24 +0300

    Effects and variables changed

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
-# Tuodaan Slide-efekti terminaltekstianimaatioita varten
-from terminaltexteffects.effects.effect_slide import Slide
+# Tuodaan Decrypt-efekti terminaltekstianimaatioita varten
+from terminaltexteffects.effects.effect_decrypt import Decrypt
 
-# Tuodaan Beams-efekti terminaltekstianimaatioita varten
-from terminaltexteffects.effects.effect_beams import Beams
+# Tuodaan Matrix-efekti terminaltekstianimaatioita varten
+from terminaltexteffects.effects.effect_matrix import Matrix
 
 # Tuodaan Fore ja Style väritekstejä varten
 from colorama import Fore, Style
@@ -12,19 +12,12 @@ import winsound
 
 # Alustetaan alkuruutu-animaation sisältö monirivisellä tekstillä
 alkuanimaatioruutu = """
-  ___ ___          .__   .__               
- /   |   \   ____  |  |  |  |    ____      
-/    ~    \_/ __ \ |  |  |  |   /  _ \     
-\    Y    /\  ___/ |  |__|  |__(  <_> )    
- \___|_  /  \___  >|____/|____/ \____/     
-       \/       \/                         
-
- __      __               .__       .___._.
-/  \    /  \ ____ _______ |  |    __| _/| |
-\   \/\/   //  _ \\_  __ \|  |   / __ | | |
- \        /(  <_> )|  | \/|  |__/ /_/ |  \|
-  \__/\  /  \____/ |__|   |____/\____ |  __
-       \/                            \/  \/
+ ___ _             ___       
+|_ _( )_ __ ___   |_ _|_ __  
+ | ||/| '_ ` _ \   | || '_ \ 
+ | |  | | | | | |  | || | | |
+|___| |_| |_| |_| |___|_| |_|
+              Hacker on board
 """
 
 # Alustetaan loppuruutu-animaation sisältö monirivisellä tekstillä
@@ -33,17 +26,13 @@ Epic Cinematic Gaming Cyberpunk | RESET by Alex-Productions | https://onsound.eu
 Music promoted by https://www.chosic.com/free-music/all/
 Creative Commons CC BY 3.0
 https://creativecommons.org/licenses/by/3.0/
-
-Sound Effect by UNIVERSFIELD from Pixabay
-https://pixabay.com/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=144751
-https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=144751
 """
 
 # Soitetaan taustamusiikki loopattuna asynkronisesti (ei pysäytä ohjelmaa)
 winsound.PlaySound("bgmusicexample.wav", winsound.SND_LOOP + winsound.SND_ASYNC)
 
 # Luodaan Slide-efekti alkuruudun animaatiota varten
-effect = Slide(alkuanimaatioruutu)
+effect = Decrypt(alkuanimaatioruutu)
 effect.effect_config.merge = True  # Määritetään, että animaatioiden kehykset sulautuvat yhteen
 
 # Animaatio toistetaan terminaaliin
@@ -66,7 +55,7 @@ print(Style.RESET_ALL)
 tmp = input("Press enter to continue...")
 
 # Luodaan Beams-efekti loppuruudun animaatiota varten
-effect = Beams(loppuanimaatioruutu)
+effect = Matrix(loppuanimaatioruutu)
 
 # Animaatio toistetaan terminaaliin
 with effect.terminal_output() as terminal:
