ec01c2422fcbf09eccc3a6fdf769c112e5c6c8bf
Tim Fabritius <tim.fabritius@skiff.com>
2024-09-19 11:19:50 +0300

    Effects and variables changed

--- game.py
+++ game.py
@@ -4,11 +4,17 @@ from terminaltexteffects.effects.effect_decrypt import Decrypt
 # Tuodaan Matrix-efekti terminaltekstianimaatioita varten
 from terminaltexteffects.effects.effect_matrix import Matrix
 
+# Tuodaan Burn-efekti terminaltextanimaatiota varten
+from terminaltexteffects.effects.effect_burn import Burn
+
 # Tuodaan Fore ja Style väritekstejä varten
 from colorama import Fore, Style
 
 # Tuodaan winsound äänentoistoa varten
 import winsound
+
+# Tuodaan os ruuduntyhjennystä varten
+import os
 
 # Alustetaan alkuruutu-animaation sisältö monirivisellä tekstillä
 alkuanimaatioruutu = """
@@ -18,10 +24,62 @@ alkuanimaatioruutu = """
  | |  | | | | | |  | || | | |
 |___| |_| |_| |_| |___|_| |_|
               Hacker on board
+ ___ _             ___       
+|_ _( )_ __ ___   |_ _|_ __  
+ | ||/| '_ ` _ \   | || '_ \ 
+ | |  | | | | | |  | || | | |
+|___| |_| |_| |_| |___|_| |_|
+              Hacker on board
+"""
+
+# Häviö animaatio
+havioanimaatioruutu = """
+
+
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                     @@@@@@@@@@@@@@@@@@@@@@@@@@                                     
+                               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                               
+                          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                          
+                       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                       
+                     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                      
+                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    
+                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    
+                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    
+                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    
+                    @@@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@     @@@@@@@@@@@@@@@@                     
+                     @@@@@@@@@               @@@@@@@@@                @@@@@@@@                      
+                       @@@@@@                 @@@@@@@@                @@@@@@                        
+                         @@@@@             @@@@@@@@@@@@@@             @@@@@                         
+                        @@@@@@@@@@@@@@@@@@@@@@@@    @@@@@@@@@@@@@@@@@@@@@@@                         
+                        @@@@@@@@@@@@@@@@@@@@@@       @@@@@@@@@@@@@@@@@@@@@@                         
+                                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                 
+                 @@@@@@@              @@@@@@@@@@@@@@@@@@@@@@@@              @@@@@@@                 
+               @@@@@@@@@@@           @@@@@@@@@@@@@@@@@@@@@@@@@@           @@@@@@@@@@@               
+               @@@@@@@@@@@@@          @@@@@@@@@@@@@@@@@@@@@@@           @@@@@@@@@@@@@               
+           @@@@@@@@@@@@@@@@@@@@@                                    @@@@@@@@@@@@@@@@@@@@@           
+          @@@@@@@@@@@@@@@@@@@@@@@@@@@@                       @@@@@@@@@@@@@@@@@@@@@@@@@@@@@          
+            @@@@@@@@@      @@@@@@@@@@@@@@@@@@@        @@@@@@@@@@@@@@@@@@@      @@@@@@@@@            
+                                    @@@@@@@@@@@@@@@@@@@@@@@@@@@                                     
+                                      @@@@@@@@@@@@@@@@@@@@@@@@                                      
+            @@@@@@@@        @@@@@@@@@@@@@@@@@@@      @@@@@@@@@@@@@@@@@@@       @@@@@@@@             
+          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@          
+           @@@@@@@@@@@@@@@@@@@@@@                                  @@@@@@@@@@@@@@@@@@@@@@           
+               @@@@@@@@@@@@@                                            @@@@@@@@@@@@@               
+               @@@@@@@@@@@                                               @@@@@@@@@@@@               
+                @@@@@@@@                                                   @@@@@@@@@                
+                                                                                                                                                                                                
 """
 
 # Alustetaan loppuruutu-animaation sisältö monirivisellä tekstillä
 loppuanimaatioruutu = """
+Epic Cinematic Gaming Cyberpunk | RESET by Alex-Productions | https://onsound.eu/
+Music promoted by https://www.chosic.com/free-music/all/
+Creative Commons CC BY 3.0
+https://creativecommons.org/licenses/by/3.0/
+
 Epic Cinematic Gaming Cyberpunk | RESET by Alex-Productions | https://onsound.eu/
 Music promoted by https://www.chosic.com/free-music/all/
 Creative Commons CC BY 3.0
@@ -40,6 +98,9 @@ with effect.terminal_output() as terminal:
     for frame in effect:
         terminal.print(frame)  # Tulostetaan animaation kukin kehys terminaaliin
 
+tmp = input("Press enter to continue")
+os.system('cls')
+
 # Tulostetaan värejä terminaaliin
 print(Fore.RED + "Red")      # Tulostetaan punainen teksti
 print(Fore.GREEN + "Green")  # Tulostetaan vihreä teksti
@@ -53,6 +114,22 @@ print(Style.RESET_ALL)
 
 # Odotetaan käyttäjän syötettä jatkamiseen
 tmp = input("Press enter to continue...")
+
+# Ruuduntyhjennys
+os.system('cls')
+
+# Luodaan Slide-efekti alkuruudun animaatiota varten
+effect = Burn(havioanimaatioruutu)
+effect.effect_config.merge = True  # Määritetään, että animaatioiden kehykset sulautuvat yhteen
+
+# Animaatio toistetaan terminaaliin
+with effect.terminal_output() as terminal:
+    for frame in effect:
+        terminal.print(frame)  # Tulostetaan animaation kukin kehys terminaaliin
+
+tmp = input("Press enter to continue...")
+
+os.system('cls')
 
 # Luodaan Beams-efekti loppuruudun animaatiota varten
 effect = Matrix(loppuanimaatioruutu)
