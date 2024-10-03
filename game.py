283e48220bf4a1b3f42b54f64795e8ff700b1fd4
skm1900 <sv.k.mattila@saunalahti.fi>
2024-10-03 15:04:34 +0300

    def travel_menu update

--- game.py
+++ game.py
@@ -253,10 +253,10 @@ def travel_menu(country_code):
 
     # print menu
     print("\nAvailable Airports: \n")
-
+    print(f"{'ICAO':<15}{'Names':<35}{'Price':<15}{'CO2(ppm)':<15}")
     for (a, b, c, d) in zip(icao, names, prices, co2):
-        print(f"{a}  {b} \n      price: {c}  CO2(ppm): {d}\n")
-
+        b = b.split()[0]
+        print(f"{a:<15}{b:<35}{c:<15}{d:<15}")
     destination = input("Where do you want to go? Please choose airport code from the list: ")
     # if airport code in airports
     if destination in icao or destination != "":
@@ -1439,7 +1439,7 @@ https://creativecommons.org/licenses/by/3.0/
 # print(tmp)
 # pay(1,0,1)
 # raiseThreat('failure')
-lowerThreat()
+#lowerThreat()
 
 # MIKON FUNKTIOT
 # mission0()
@@ -1456,7 +1456,7 @@ lowerThreat()
 # pauseMenu()
 
 # Svetlanan funktiot
-# travel_menu("FI")
+travel_menu("FI")
 # travel_to("EFHK")
 
 # PÄÄOHJELMA
